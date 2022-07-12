b8fd53b2a2951761d550d14a1d3964b79c17897e
pratiksj <pratiksharai728@gmail.com>
2022-07-12 13:31:52 +0545

    make changes in react

--- part1/noteApp/components/App.js
+++ part1/noteApp/components/App.js
@@ -8,15 +8,29 @@ export default App;*/
 
 import React from "react";
 import Myh1 from "./Myh1";
+import React, { useState } from "react";
 
-const App = () => {
+const App = ({ counter }) => {
+  const [myCount, setMyCount] = useState(1);
+
+  setTimeout(() => {
+    setMyCount(myCount + 1);
+  }, 1000);
   let name1 = "pratiksha";
+  console.log(name1);
+
   return (
     <div>
-      <h1>hellow {name1} </h1>
-      <Myh1 name={name1} lastName="somelastname" />
-      <Myh1 name="niru" lastName="Magar" />
-      <Myh1 name="manjila" lastName="Nepali" />
+      <h1>hellow {myCount} </h1>
+      <h1>{counter}</h1>
+      <Myh1
+        name={name1}
+        lastName="somelastname"
+        count={myCount}
+        count1={counter}
+      />
+      {/* <Myh1 name="niru" lastName="Magar" count={myCount} />
+      <Myh1 name="manjila" lastName="Nepali" count={myCount} /> */}
     </div>
   );
 };
--- part1/noteApp/components/Myh1.js
+++ part1/noteApp/components/Myh1.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-const Myh1 = ({ name, lastName }) => {
+const Myh1 = ({ name, lastName, count, count1 }) => {
   //spread gareko already object vitra ko key matra deu vaneko
   //distructure gareko
   //props garnu parena
   //return React.createElement("h1", {}, "This is from REACT H1 Componet");
+  //console.log(name, lastName, count, count1);
   return (
-    <h1>
-      helow {name} {lastName}
-    </h1>
+    <div>
+      <h1>
+        {/* helow to tej {name} {lastName}
+      helow to tej {name} {lastName} {count} */}
+        pratikshya {count}
+      </h1>
+
+      <h1>{count1}</h1>
+    </div>
   );
 };
 export default Myh1;
