8f625bd3fa51c260dacf05d7c5e2fbe7f356cc98
pratiksj <pratiksharai728@gmail.com>
2022-07-13 13:56:38 +0545

    done remaining lesson of react

--- part1/noteApp/components/App.js
+++ part1/noteApp/components/App.js
@@ -9,9 +9,12 @@ export default App;*/
 import React from "react";
 import Myh1 from "./Myh1";
 import React, { useState } from "react";
+import HistoryDisplay from "./HistoryDisplay";
+import Mybutton from "./Mybutton";
 
 const App = ({ counter }) => {
   const [myCount, setMyCount] = useState(1);
+  const [clickButtonHistory, setClickButtonHistory] = useState([]);
 
   //const increaseCount = () => setMyCount(myCount + 1);
 
@@ -19,13 +22,21 @@ const App = ({ counter }) => {
   //setMyCount(myCount + 1);
   //}, 1000);
   let name1 = "pratiksha";
-  let increment = () => setMyCount(myCount + 1);
-  let decrement = () => setMyCount(myCount - 1);
-
+  //let increment = () => setMyCount(myCount + 1);
+  const increaseCountFromApp = () => {
+    setMyCount(myCount + 1);
+    setClickButtonHistory(clickButtonHistory.concat("App"));
+  };
+  const increaseCountFromButton = () => {
+    setMyCount(myCount - 1);
+    setClickButtonHistory([...clickButtonHistory, "Button"]);
+  };
   return (
     <div>
-      <button onClick={increment}>increase</button> &nbsp; &nbsp;
-      <button onClick={decrement}>decrease</button>
+      <HistoryDisplay history={clickButtonHistory} />
+      <button onClick={increaseCountFromApp}>increase</button> &nbsp; &nbsp;
+      {/* <button onClick={decrement}>decrease</button> */}
+      <Mybutton decrease={increaseCountFromButton}>decrease</Mybutton>
       <h1>
         hellow {name1} {myCount}
       </h1>
---
+++ part1/noteApp/components/HistoryDisplay.js
@@ -0,0 +1,4 @@
+const HistoryDisplay = ({ history }) => {
+  return <h1>This click will show the history:{history}</h1>;
+};
+export default HistoryDisplay;
