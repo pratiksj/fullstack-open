9a2a679270dcaa17493147f8498f06462cb2cc62
pratiksj <pratiksharai728@gmail.com>
2022-07-13 14:03:06 +0545

    set  condition of history display of react

--- part1/noteApp/components/HistoryDisplay.js
+++ part1/noteApp/components/HistoryDisplay.js
@@ -1,4 +1,8 @@
 const HistoryDisplay = ({ history }) => {
-  return <h1>This click will show the history:{history}</h1>;
+  if (history.length > 0) {
+    return <h1>This click will show the history:{history}</h1>;
+  } else {
+    return <h1>This click will not show the history yet</h1>;
+  }
 };
 export default HistoryDisplay;
