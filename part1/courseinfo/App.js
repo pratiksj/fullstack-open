53f457184a8e12ad4adb52fd82d05e5c899e5336
pratiksj <pratiksharai728@gmail.com>
2022-07-13 16:10:11 +0545

    complete the courseinfo exercise of react

--- part1/courseinfo/App.js
+++ part1/courseinfo/App.js
@@ -2,27 +2,29 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header heading={course} />
-      <Content parts={parts} />
+      <Header heading={course.name} />
+      <Content parts={course.parts} />
 
-      <Total parts={parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
