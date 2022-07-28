5d7c84214a577f4426b266eccc26a2304c013110
pratiksj <pratiksharai728@gmail.com>
2022-07-28 13:43:56 +0545

    start courseinfo react

--- part1/anecdotes/src/Mostvote.js
+++ part1/anecdotes/src/Mostvote.js
@@ -1,7 +1,7 @@
 const MostVote = (props) => {
   let higherVote = Math.max(...props.newVote); //newvote array ma xa spread le chai array ma vako element lai matra liyo
   let indexOfHigherVote = props.newVote.indexOf(higherVote); //Math.max le chaii high element lai linxa as a number
-  let dote = props.anec[indexOfHigherVote];
+  let dote = props.anec[indexOfHigherVote]; //highert vote vako statement print garna ko lagi ho
 
   if (higherVote === 0) {
     return <p>No votes yet</p>;
