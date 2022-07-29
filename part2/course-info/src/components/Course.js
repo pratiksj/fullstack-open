fd092bc1f61619489eeb2487e24e847366cc1ff2
pratiksj <pratiksharai728@gmail.com>
2022-07-29 11:11:38 +0545

    refactor the code with addtional object

--- part2/course-info/src/components/Course.js
+++ part2/course-info/src/components/Course.js
@@ -1,25 +1,40 @@
 //import Part from "./Part";
+import Header from "./Header";
 const Course = ({ course }) => {
   //const result = course.parts.map((note) => note.name);
   //console.log(result);
   return (
     <>
-      <h1>{course.name}</h1>
-      <ul>
-        {course.parts.map((note) => (
-          <li key={note.id}>
+      <Header head={"Web developement Curriculum"} />
+      <h1>{course[0].name}</h1>
+      <>
+        {course[0].parts.map((note) => (
+          <p key={note.id}>
             {note.name}
             {note.exercises}
-          </li>
+          </p>
         ))}
-      </ul>
-      <p>
+      </>
+      <h1>
         total of
-        {course.parts.reduce((s, p) => {
+        {course[0].parts.reduce((s, p) => {
           return s + p.exercises;
         }, 0)}
         exercises
-      </p>
+      </h1>
+      <h2>{course[1].name}</h2>
+      <>
+        {course[1].parts.map((x) => (
+          <p key={x.id}>
+            {x.name}
+            {x.exercises}
+          </p>
+        ))}
+      </>
+      <h1>
+        total of
+        {course[1].parts.reduce((a, b) => a + b.exercises, 0)} exercises
+      </h1>
 
       {/* <Part parts={props.parts} /> */}
     </>
