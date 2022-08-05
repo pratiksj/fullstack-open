09925242b33dad5578bc03911460949e1a5ea731
pratiksj <pratiksharai728@gmail.com>
2022-08-05 16:00:49 +0545

    refactor the code

--- part1/noteApp/components/HistoryDisplay.js
+++
@@ -1,8 +0,0 @@
-const HistoryDisplay = ({ history }) => {
-  if (history.length > 0) {
-    return <h1>This click will show the history:{history}</h1>;
-  } else {
-    return <h1>This click will not show the history yet</h1>;
-  }
-};
-export default HistoryDisplay;
