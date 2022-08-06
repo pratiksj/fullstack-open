319aef0fc6105b1cac61d576677b4992fd3fd283
pratiksj <pratiksharai728@gmail.com>
2022-08-06 10:17:36 +0545

    refactor application using object as well as adjusted with respect to previous component

--- part1/course-info/src/components/Course.js
+++ part1/course-info/src/components/Course.js
@@ -1,6 +1,8 @@
 import Part from "./Part";
 
 const Course = (props) => {
+  //console.log(props);
+  //console.log(props.part1.name);
   return (
     <>
       <Part part={props.part1} exercise={props.exercise1} />
